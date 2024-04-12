f21fcac3d4d9c6f0cc8d0a21159d17262442161f
minhtien213 <minhtien213@gmail.com>
2024-04-12 14:19:07 +0900

    fix authMiddleware

--- src/routes/CommentRouter.js
+++ src/routes/CommentRouter.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const CommentController = require('../app/controllers/CommentController');
 const { authMiddleware, authUserMiddleware } = require('../app/middlewares/authMiddleware');
 
-router.post('/create-comment/:id', CommentController.createComment);
+router.post('/create-comment/:id', authUserMiddleware, CommentController.createComment);
 router.get('/get-comment/:id', CommentController.getComment);
-router.delete('/remove-comment/:id/:commentId', CommentController.removeComment);
+router.delete(
+  '/remove-comment/:id/:commentId',
+  authUserMiddleware,
+  CommentController.removeComment,
+);
 
 module.exports = router;
+
+//thêm access_token
--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const orderController = require('../app/controllers/OrderController');
 const { authMiddleware, authUserMiddleware } = require('../app/middlewares/authMiddleware');
 
-router.post('/create-order/:id', orderController.createOrder);
+router.post('/create-order/:id', authUserMiddleware, orderController.createOrder);
 
-router.get('/get-my-orders/:id', orderController.getMyOrders);
+router.get('/get-my-orders/:id', authUserMiddleware, orderController.getMyOrders);
 
 module.exports = router;
